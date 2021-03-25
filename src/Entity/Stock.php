93a8a29eedff5fae2ed65a894a46ce3e136da115
Liam Powell <ljpbsod@gmail.com>
2021-03-25 16:30:02 +0100

    add starter entites and add some dependencies

---
+++ src/Entity/Stock.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StockRepository::class)
+ */
+class Stock
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $item;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $count;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItem(): ?string
+    {
+        return $this->item;
+    }
+
+    public function setItem(string $item): self
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+}
